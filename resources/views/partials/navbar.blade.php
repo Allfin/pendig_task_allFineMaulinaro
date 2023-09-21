4e12f7fb376bafc60af8599a87f53539d3d141a7
Allfin <maulinaro@yahoo.com>
2023-09-22 06:43:57 +0700

    Setup Bootstrap, Navbar and Main Pages

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+	<div class="container">
+		<a
+			class="navbar-brand"
+			href="/"
+		>ALLFine Blog</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNavAltMarkup"
+			aria-controls="navbarNavAltMarkup"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div
+			class="navbar-collapse collapse"
+			id="navbarNavAltMarkup"
+		>
+			<div class="navbar-nav">
+				<a
+					class="nav-link {{ $title === 'Home' ? 'active' : '' }}"
+					href="/"
+				>Home</a>
+				<a
+					class="nav-link {{ $title === 'Post' ? 'active' : '' }}"
+					href="/posts"
+				>Blog</a>
+				<a
+					class="nav-link {{ $title === 'Category' ? 'active' : '' }}"
+					href="/categories"
+				>Category</a>
+			</div>
+		</div>
+	</div>
+</nav>
