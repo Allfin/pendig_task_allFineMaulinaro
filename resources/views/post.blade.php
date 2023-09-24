5dbcb9bc77f625a3001bad9437bb2b266fe99f8b
Allfin <maulinaro@yahoo.com>
2023-09-24 11:46:26 +0700

    fixing style All Post & Single Post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-	<h1 class="mb-5">Halaman Post</h1>
-	<h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
-	<p><a href="/categories/">category</a>/<a href="/categories/{{ $post->category->slug }}">{{ $post->category->slug }}</a>
-	</p>
-	<p>{!! $post['body'] !!}</p>
-	<a href="/posts">Go Back Post</a>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<h2>{{ $post['title'] }}</h2>
+			<p><a href="/categories/">category</a>/<a href="/categories/{{ $post->category->slug }}">{{ $post->category->slug }}</a>
+			</p>
+
+			<img
+				src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}"
+				class="card-img-top"
+				alt="{{ $post->category->name }}"
+			>
+
+			<p>{!! $post['body'] !!}</p>
+			<a href="/posts">Go Back Post</a>
+		</div>
+	</div>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,43 @@
 
 @section('container')
 	<h1 class="mb-5">{{ $title }}</h1>
-	@foreach ($posts as $post)
-		<h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
-		<p>{{ $post['excerpt'] }}</p>
-	@endforeach
+	<div class="row">
+		@foreach ($posts as $post)
+			<div class="col-md-4 mb-3">
+				<div class="card">
+					<div
+						class="position-absolute px-3 py-2 text-white"
+						style="background-color: rgba(0, 0, 0, 0.7)"
+					><a
+							href="/categories/{{ $post->category->slug }}"
+							class="text-decoration-none text-white"
+						>{{ $post->category->name }}</a></div>
+					<a
+						href="/posts/{{ $post->slug }}"
+						class="text-decoration-none"
+					>
+						<img
+							src="https://source.unsplash.com/500x300/?{{ $post->category->name }}"
+							class="card-img-top"
+							alt="{{ $post->category->name }}"
+						>
+					</a>
+					<div class="card-body">
+						<h5 class="card-title"><a
+								href="/posts/{{ $post->slug }}"
+								class="text-decoration-none link-dark"
+							>{{ $post->title }}</a>
+						</h5>
+						<p class="card-text">{{ $post->excerpt }}</p>
+						<a
+							href="/posts/{{ $post->slug }}"
+							class="btn btn-primary"
+						>Read More</a>
+					</div>
+				</div>
+			</div>
+			{{-- <h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
+      <p>{{ $post['excerpt'] }}</p> --}}
+		@endforeach
+	</div>
 @endsection
